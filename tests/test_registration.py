916b2235b81333e8daf44ab3135e702ba00f3f7d
Regina2911 <reginalee9@mail.ru>
2026-06-06 11:23:33 +0200

    Изменения

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -39,7 +39,8 @@ def test_add_new_user(setup_database, connection):
 def test_add_new_existing_name_user(setup_database, connection):
     # Тест добавления пользователя с существующим именем.
     add_user('testuser', 'testuser@example.com', 'password123')
-    assert not add_user('testuser', 'testuser2@example.com', 'password456') is False
+    assert not add_user('testuser', 'testuser2@example.com', 'password456') 
+    
 
 def test_successful_authentication(setup_database, connection):
     """Тест успешной аутентификации пользователя."""
